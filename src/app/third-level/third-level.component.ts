7619ff475ed9f82987b0d68df208572d735cf95e
bandikedivya <divyacrkdv@gmail.com>
2025-08-12 11:41:26 +0530

    code added

---
+++ src/app/third-level/third-level.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-third-level',
+  templateUrl: './third-level.component.html',
+  styleUrls: ['./third-level.component.css']
+})
+export class ThirdLevelComponent {
+  checkEligibility = true
+}
