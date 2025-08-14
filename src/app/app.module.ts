4184386a4f2bf7ecc951ecf387000b14c2df7a18
bandikedivya <divyacrkdv@gmail.com>
2025-08-14 14:42:33 +0530

    content control techniques code added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { FirstlevelComponent } from './firstlevel/firstlevel.component';
 import { SecondLevelComponent } from './second-level/second-level.component';
 import { ThirdlevelComponent } from './thirdlevel/thirdlevel.component';
+import { FourthLevelComponent } from './fourth-level/fourth-level.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { ThirdlevelComponent } from './thirdlevel/thirdlevel.component';
     TestComponent,
     FirstlevelComponent,
     SecondLevelComponent,
-    ThirdlevelComponent
+    ThirdlevelComponent,
+    FourthLevelComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/fourth-level/fourth-level.component.ts
@@ -0,0 +1,63 @@
+import { Component ,ContentChild,ElementRef,TemplateRef,ViewChild, ViewContainerRef } from '@angular/core';
+// Should import ViewChild  
+
+
+@Component({
+  selector: 'app-fourth-level',
+  templateUrl: './fourth-level.component.html',
+  styleUrls: ['./fourth-level.component.css']
+})
+export class FourthLevelComponent {
+  //@View Child
+//Syntax: @ViewChild("Element Reference from html") localref: type(any);
+
+
+
+
+//@ViewChild('firstName') userfirstname: any; // should give string in single or double qoutes.
+
+@ViewChild('firstName') userfirstname: ElementRef<any>; // should give string in single or double qoutes.
+//It will throw error for userfirstname, so should add "strictPropertyInitialization" = false; in tsconfig.json file
+
+
+@ViewChild ('mgs') TextMgs : ElementRef<any>; // Ref for <p>
+
+
+
+// Sometime, we'll give content in the <app-fourth-level> in app.component.html. So, we give reference in <app-fourth-level> and use this @ContantChild in fourth-level.component.ts;
+
+@ContentChild('childmgs1') contentmgs1 : ElementRef<any>;
+@ContentChild('childmgs2') contentmgs2 : ElementRef<any>;
+@ContentChild('text3')  contentmgs3 : ElementRef<any>;
+
+btnClick()
+{
+  console.log(this.TextMgs); //prints the type in console
+   console.log(this.TextMgs.nativeElement.innerText); //prints the type in console
+   console.log(this.userfirstname);  //prints the type in console
+  console.log(`Entered Value is: ${this.userfirstname.nativeElement.value}`);  //prints the type in console
+  console.log(this.contentmgs1.nativeElement.innerText);
+  console.log(this.contentmgs2.nativeElement.innerText);
+  console.log(this.contentmgs3.nativeElement.innerText);
+}
+
+
+
+
+NumberList = [1,2,3,4,5];
+
+
+
+
+@ViewChild ('tempCom',{read: TemplateRef}) tempCont: TemplateRef<any>;
+
+@ViewChild('container', {read:ViewContainerRef}) container:ViewContainerRef;
+
+  etnclick()
+  {
+    //console.log(this.tempCont);
+    //this.container.insert(this.tempCont.createEmbeddedView(null));
+    // instead of null, we can pass any value
+    this.container.insert(this.tempCont.createEmbeddedView({'firstName' : "Bandike"}));
+  }
+}
